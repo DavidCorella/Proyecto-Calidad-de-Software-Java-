b6e568314736c71a2acd644b2fc6074b249a5ef5
DavidCorella <davidcorella.sanchez2511@gmail.com>
2023-11-25 14:58:47 -0600

    Arranque automatico de Interfaz Grafica

--- SistemaDeInventario/src/sistemadeinventario/SistemaDeInventario.java
+++ SistemaDeInventario/src/sistemadeinventario/SistemaDeInventario.java
@@ -1,22 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package sistemadeinventario;
 
-/**
- *
- * @author Jefferson
- */
+import Vista.Principal;
+
+
 public class SistemaDeInventario {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        //Hola
+        Principal mainWindow = new Principal();
+        mainWindow.setVisible(true);
     }
     
 }
